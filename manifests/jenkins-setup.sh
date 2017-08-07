915ff9a476ca655ce250b3e22f9e88de18b98644
Marc-Velay <marc.velay@gmail.com>
2017-08-07 18:04:44 +0200

    redid dockerfile 18

--- manifests/jenkins-setup.sh
+++ manifests/jenkins-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
-exec java -jar jenkins.jar 
+exec java -jar /jenkins.war 
