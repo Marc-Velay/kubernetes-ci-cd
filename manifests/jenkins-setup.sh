d5148c3100c394a9fcf2729cad368c8bcfe16147
Marc-Velay <marc.velay@gmail.com>
2017-08-07 16:57:32 +0200

    redid dockerfile 13

--- manifests/jenkins-setup.sh
+++ manifests/jenkins-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
-exec addgroup jenkins
-exec su -c "useradd jenkins -s /bin/bash -m -g $PRIMARYGRP -G $MYGROUP -G jenkins"
-exec /sbin/setuser jenkins java -jar jenkins.war 
+exec addgroup jenkins && \
+    su -c "useradd jenkins -s /bin/bash -m -g $PRIMARYGRP -G $MYGROUP -G jenkins" && \
+    /sbin/setuser jenkins java -jar jenkins.war 
